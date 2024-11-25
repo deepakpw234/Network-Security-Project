dfa2eec513f8eeb6bb99481eb1f6db61c9cc12c0
deepakpw234 <deepakpw234@gmail.com>
2024-11-25 23:30:55 +0530

    data validation

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ import sys
 
 
 from src.components.data_ingestion import DataIngestion
-from src.entity.config_entity import DataIngestionConfig
+from src.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from src.entity.config_entity import TrainingPipelineConfig
 
 from src.logging.logger import logging
 from src.exception.exception import CustomException
 
+from src.components.data_validation import DataValidation
+
 if __name__=="__main__":
     try:
-        logging.info("Data ingestion started")
+        
         trainpipelineconfig = TrainingPipelineConfig()
         dataingestionconfig = DataIngestionConfig(trainpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
-        data_ingestion.initiate_data_ingestion()
-        logging.info("Data ingestion Complated")
+        logging.info("Data ingestion started")
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        print(data_ingestion_artifact)
+        logging.info("Data ingestion Completed")
+
+        data_validation_config = DataValidationConfig(trainpipelineconfig)
+        data_validation = DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info("Data validation started")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        print(data_validation_artifact)
+        logging.info("Data validation completed")
+
+
     except Exception as e:
         raise CustomException(e,sys)
---
+++ src/components/data_validation.py
@@ -0,0 +1,125 @@
+import os
+import sys
+import pandas as pd
+
+from src.exception.exception import CustomException
+from src.logging.logger import logging
+
+
+from src.entity.config_entity import DataValidationConfig
+from src.entity.artifact_entity import DataIngestionartifact,DataValidationartifact
+from scipy.stats import ks_2samp   # for checking the distribution
+
+
+from src.constant.training_pipeline import SCHEMA_FILE_PATH
+from src.utils.main_utils.utils import read_yaml_file, write_yaml_file
+
+
+class DataValidation:
+    def __init__(self,data_ingestion_artifact:DataIngestionartifact,
+                 data_validation_config:DataValidationConfig):
+        try:
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_validation_config = data_validation_config
+            self._schema_config = read_yaml_file(SCHEMA_FILE_PATH)
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    @staticmethod    
+    def read_data(file_path)->pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def validate_no_of_columns(self,dataframe:pd.DataFrame)->bool:
+        try:
+            no_of_columns = len(self._schema_config['columns'])
+            logging.info(f"required columns are {no_of_columns}")
+            logging.info(f"dataframe has columns {len(dataframe.columns)}")
+
+            if len(dataframe.columns) == no_of_columns:
+                return True
+            else:
+                return False
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def detect_dataset_drift(self,base_dataframe,current_dataframe,threshold=0.05)->bool:
+        try:
+            status = True
+            report = {}
+            for column in base_dataframe.columns:
+                d1 = base_dataframe[column]
+                d2 = current_dataframe[column]
+                is_same_dist = ks_2samp(d1,d2)
+                if threshold <= is_same_dist.pvalue:
+                    is_found = False
+                else:
+                    is_found = True
+                    status = False
+
+                report.update({column:{
+                    "p-value":float(is_same_dist.pvalue),
+                    "drift_status": is_found
+                }})
+
+            drift_report_file_path = self.data_validation_config.drift_report_file_path
+
+            # creating the directory
+            dir_path = os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+
+            write_yaml_file(file_path=drift_report_file_path,content=report)
+
+            return status
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+    def initiate_data_validation(self)->DataValidationartifact:
+        try:
+            self.train_file_path = DataIngestionartifact.train_file_path
+            self.test_file_path = DataIngestionartifact.test_file_path
+
+            # read data from train and test
+            train_dataframe = DataValidation.read_data(self.train_file_path)
+            test_datafrane = DataValidation.read_data(self.test_file_path)
+
+            # validate number of columns
+            status = self.validate_no_of_columns(train_dataframe)
+            if status==False:
+                print(f"Train dataframe does not contain all columns")
+            else:
+                print("euqual column")          
+
+            status = self.validate_no_of_columns(test_datafrane)
+            if status==False:
+                print(f"Test dataframe does not contain all columns")
+
+            # check data drift
+            status = self.detect_dataset_drift(train_dataframe,test_datafrane)
+            dir_path = os.path.dirname(self.data_validation_config.valid_train_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            
+            train_dataframe.to_csv(self.data_validation_config.valid_train_file_path,header=True,index=False)
+
+            test_datafrane.to_csv(self.data_validation_config.valid_test_file_path,header=True,index=False)
+
+            datavalidationartifact = DataValidationartifact(
+                validation_status=status,
+                valid_train_file_path=self.data_validation_config.valid_train_file_path,
+                valid_test_file_path=self.data_validation_config.valid_test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path
+            )
+
+            return datavalidationartifact
+
+        except Exception as e:
+            raise CustomException(e,sys)
