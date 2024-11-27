1fa7b409e945f9dda8b09f0b50b63c046c859184
deepakpw234 <deepakpw234@gmail.com>
2024-11-27 13:19:28 +0530

    batch prediction FastAPI

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import sys
 from src.exception.exception import CustomException
 from src.logging.logger import logging
 from src.pipelines.train_pipeline import TrainingPipeline
+from src.utils.main_utils.utils import load_pickle_object
+
+from src.utils.ml_utils.model.estimator import NetworkData
 
 import certifi
 ca = certifi.where()
@@ -17,7 +20,7 @@ print(mongo_db_url)
 import pymongo
 
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI,requests,UploadFile,File
+from fastapi import FastAPI,Request,UploadFile,File
 from fastapi.responses import Response
 from uvicorn import run as app_run
 from starlette.responses import RedirectResponse
@@ -41,6 +44,10 @@ app.add_middleware(
     allow_headers = ["*"],
 )
 
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="./templates")
+
+
 @app.get("/",tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
@@ -56,6 +63,32 @@ async def train_route():
     except Exception as e:
         raise CustomException(e,sys)
     
+@app.post("/predict")
+async def predict_route(request:Request,file:UploadFile = File(...)):
+    try:
+        df = pd.read_csv(file.file)
+        # print(df)
+
+        # loading preprocessor and model
+        preprocessor = load_pickle_object('final_model/preprocessor.pkl')
+        model = load_pickle_object('final_model/model.pkl')
+
+        network_model = NetworkData(preprocessor=preprocessor,model=model)
+
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predtiction'] = y_pred
+        print(df['predtiction'])
+
+        df.to_csv("prediction_output/output.csv")
+
+        table_html = df.to_html(classes="table table-striped")
+        # print(table_html)
+        return templates.TemplateResponse("table.html", {"request":request,"table":table_html})
+    
+
+    except Exception as e:
+        raise CustomException(e,sys)    
 
 if __name__=="__main__":
     app_run(app,host="loacalhost",port=8000)
